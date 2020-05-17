cbf79b705e04807cb875c2e30eb077a89684b381
momolly1024 <momolly1024@gmail.com>
2020-05-17 19:57:51 +0800

    換一個API確認問題

--- index.js
+++ index.js
@@ -10,18 +10,26 @@ const bot = linebot({
 })
 
 // 當收到訊息時
-bot.on('message', event => {
+bot.on('message', async (event) => {
   let msg = ''
   try {
-    const data = rp({
-      uri: 'https://api.avgle.com/v1/collections/[1, 250]', json: true
-    })
-    msg += data.response.collections[0].title
+    const data = await rp({ url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=0d93c386d9d8221cbfa4c29585d53c53&language=zh-tw', json: true })
+    if (event.message.text === 'now_playing') {
+      for (let i = 0; i < data.results.length; i++) {
+        // movie += data.result[i].original_title
+        msg += data.results[i].original_title + '\n'
+      }
+    } else if (event.message.text === '熱映中') {
+      for (let i = 0; i < data.results.length; i++) {
+        msg += data.results[i].title + '\n'
+      }
+    } else {
+      msg = '哈囉尼豪~~ \n請輸入【now_playing】或【熱映中】，查看近期熱播中的電影！\n好啦還是你想知道【機器人在哪】、【機器人長相】、【滾】、【簡介】(僅顯示前五部電影簡介)、【...】'
+    }
   } catch (error) {
     msg = '發生錯誤'
   }
   event.reply(msg)
-  console.log(msg)
 })
 
 // 啟動
